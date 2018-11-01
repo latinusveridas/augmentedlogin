818b7453af15edb210799ae8c801732394f07976
John D <sparteclaudius749@gmail.com>
2018-11-01 11:26:15 +0100

    amebded db credentials

--- Database/database.js
+++ Database/database.js
@@ -3,10 +3,10 @@ var mysql = require('mysql');
 //DEFINE POOLING
 var pool = mysql.createPool({
     connectionLimit: 10,
-    host: process.env.OPENSHIFT_MYSQL_DB_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE
+    host: "localhost",
+    user: "root",
+    password: "Miroslava326356$$$$$",
+    database: "sampledb"
 });
 
 
