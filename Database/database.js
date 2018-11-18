34552104fddad6b74d693276a6dbe4cc61227052
John D <sparteclaudius749@gmail.com>
2018-11-18 16:28:52 +0100

    LuxDB

--- Database/database.js
+++ Database/database.js
@@ -3,7 +3,7 @@ var mysql = require('mysql');
 //DEFINE POOLING
 var pool = mysql.createPool({
     connectionLimit: 10,
-    host: "localhost",
+    host: "83.217.132.102",
     user: "root",
     password: "Miroslava326356$$$$$",
     database: "sampledb"
